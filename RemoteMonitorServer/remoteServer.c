19fd74580294d34c672080e5eed3fc337788c77b
stanleychang <homicide522@gmail.com>
2023-06-26 20:37:05 +0800

    Integrate OpenCV libraries into remote server.

--- RemoteMonitorServer/remoteServer.c
+++ RemoteMonitorServer/remoteServer.c
@@ -1,30 +1,44 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <opencv2/opencv.hpp>
 
 #include "mysqlServer.h"
 
+using namespace cv;
+
 int main(int argc, char *argv[])
 {
-	MYSQL mysqlObj;
+	//MySQL Processing
+	{
+		MYSQL mysqlObj;
 
-	const char *queryString = "SELECT * FROM staff";
+		const char *queryString = "SELECT * FROM staff";
 
-	char userName[]           = "Larson Chang";
-	int userAge               = 55;
-	char timeRecord[]         = "2022-11-23 19:22:12";
-	char sqlInsertString[256] = {0};
+		char userName[]           = "Larson Chang";
+		int userAge               = 55;
+		char timeRecord[]         = "2022-11-23 19:22:12";
+		char sqlInsertString[256] = {0};
 
-	sprintf(sqlInsertString, "INSERT INTO staff(userName, userAge, timeRecord) VALUES('%s', %d, '%s')",
-		userName, userAge, timeRecord);
+		sprintf(sqlInsertString, "INSERT INTO staff(userName, userAge, timeRecord) VALUES('%s', %d, '%s')",
+			userName, userAge, timeRecord);
 
-	mysqlServerInit(&mysqlObj);
+		mysqlServerInit(&mysqlObj);
 
-	displayAllRecords(&mysqlObj, queryString);
+		displayAllRecords(&mysqlObj, queryString);
 
-	/*addNewRecord(&mysqlObj, sqlInsertString);
-	displayAllRecords(&mysqlObj, queryString);*/
+		//addNewRecord(&mysqlObj, sqlInsertString);
+		//displayAllRecords(&mysqlObj, queryString);
 
-	mysqlServerClose(&mysqlObj);
+		mysqlServerClose(&mysqlObj);
+	}
+
+	//OpenCV Processing
+	{
+		Mat sourceImage = imread("/home/stanleychang/stanleychangFiles/images/Yuju1-z.jpg");
+
+		imshow("GFriend Image", sourceImage);
+		waitKey(0);
+	}
 
 	exit(EXIT_SUCCESS);
 
